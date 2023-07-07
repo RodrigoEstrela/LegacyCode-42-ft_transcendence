3f4db32c555a338da5adc87bf33f0cd6e63cbe54
rodrigo estrela <rdas-nev@rdas-nev.42.fr>
2023-07-07 10:17:46 +0100

    basic nestjs app

---
+++ backend/my-nestjs-02/src/app.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+	return 'Hello World!<br>Hallo Welt!<br>Ola Mundo! \
+<br><br>Teste1<br>Teste2';
+  }
+}
