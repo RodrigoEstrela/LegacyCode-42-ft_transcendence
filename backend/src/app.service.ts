bfbadc45652efe78e128f55a330236f92512f10a
rodrigo estrela <rdas-nev@rdas-nev.42.fr>
2023-07-11 15:34:34 +0100

    potgresql

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@nestjs/common';
 export class AppService {
   getHello(): string {
 	return 'Hello World!<br>Hallo Welt!<br>Ola Mundo! \
-<br><br>Teste1<br>Teste2';
+<br><br>Teste1<br>Teste2<br>';
   }
 }
