76b0847367c0abedf1e508bd80c3cbe99f62c48d
pulgamecanica <pulgamecanica11@gmail.com>
2023-09-19 12:53:23 +0200

    Seeding review factories and seed

--- backend/src/seeding/factories/user.factory.ts
+++ backend/src/seeding/factories/user.factory.ts
@@ -1,43 +1,45 @@
-// import faker from "@faker-js/faker";
-// import { define } from 'typeorm-seeding';
-// import { User } from '../../entities';
-// import { UserStats } from '../../user';
+// Pulga review 18/09/22 -> OK
 
-// define(User, (faker) => {
-//   const username = faker.internet.userName(7);
-//   const email = faker.internet.email(14);
-//   const password = faker.internet.password(7);
+import faker from "@faker-js/faker";
+import { define } from 'typeorm-seeding';
+import { User } from '../../entities';
+import { UserStats } from '../../user';
 
-//   // You can generate random user stats or set them to initial values
+define(User, (faker) => {
+  const username = faker.internet.userName(7);
+  const email = faker.internet.email(14);
+  const password = faker.internet.password(7);
 
-//   const wins = faker.random.number(7);
-//   const losses = faker.random.number(7);
-//   const gamesPlayed = wins + losses;
-//   var score = (wins * 10) - (losses * 5);
-//   if (score < 0) {
-// 	score = 0;
-//   }
-//   const stats: UserStats = {
-//     'Games Played': gamesPlayed,
-//     'Wins': wins,
-//     'Losses': losses,
-//     'Score': score,
-//     'Rank': "",
-//     'Achievements': "",
-//   };
+  // You can generate random user stats or set them to initial values
 
-//   const user = new User();
-//   user.username = username;
-//   user.email = email;
-//   user.password = password;
-//   user.avatar = "";
-//   user.friends = [];
-//   user.friendRequestsReceived = [];
-//   user.friendRequestsSent = [];
-//   user.blockedUsers = [];
-//   user.stats = stats;
-//   user.history = [];
-//   user.status = "online";
+  const wins = faker.random.number(7);
+  const losses = faker.random.number(7);
+  const gamesPlayed = wins + losses;
+  var score = (wins * 10) - (losses * 5);
+  if (score < 0) {
+		score = 0;
+  }
+  const stats: UserStats = {
+    'Games Played': gamesPlayed,
+    'Wins': wins,
+    'Losses': losses,
+    'Score': score,
+    'Rank': "",
+    'Achievements': "",
+  };
 
-//   return user;
-// });
+  const user = new User();
+  user.username = username;
+  user.email = email;
+  user.password = password;
+  user.avatar = "";
+  user.friends = [];
+  user.friendRequestsReceived = [];
+  user.friendRequestsSent = [];
+  user.blockedUsers = [];
+  user.stats = stats;
+  user.history = [];
+  user.status = "online";
+
+  return user;
+});
--- backend/src/seeding/seeds/initalSeed.ts
+++ backend/src/seeding/seeds/initalSeed.ts
@@ -1,8 +1,19 @@
-// import { Factory, Seeder } from "typeorm-seeding";
-// import { User } from "../../entities";
+// Pulga review 18/09/22 -> NOT OK
+/**
+ * Review
+ * 1.
+ * Should declare the class function as described on the documentation
+ * 	See: [https://www.npmjs.com/package/typeorm-seeding#seeder]
+ * 2.
+ * Must import Connection from typeorm
+ **/
 
-// export default class InitialDatabaseSeed implements Seeder {
-//   public async run(factory: Factory): Promise<void> {
-//     const users = await factory(User)().createMany(7);
-//   }
-// }
+import { Connection } from "typeorm"
+import { Factory, Seeder } from "typeorm-seeding";
+import { User } from "../../entities";
+
+export default class InitialDatabaseSeed implements Seeder {
+  public async run(factory: Factory, connection: Connection): Promise<any> {
+  	await factory(User)().createMany(7);
+  }
+}
