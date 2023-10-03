9ae6009b2eb0528ac6187d88f4be672d20c940ed
rdas-nev <restrela20@gmail.com>
2023-10-03 16:28:21 +0100

    com o chat do francisco

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,48 +1,124 @@
 import React, { useEffect, useState } from 'react';
+import io, { Socket } from 'socket.io-client'; // Import socket.io-client and the Socket type
+import './styles.css'; // Import the CSS file
+
+interface MessageData {
+  senderId: string;
+  message: string;
+}
+
 const App: React.FC = () => {
-  const [messages, setMessages] = useState<string[]>([]);
+  const [messages, setMessages] = useState<MessageData[]>([]);
   const [inputValue, setInputValue] = useState('');
-  const socket = new WebSocket('ws://localhost:5050');
-  socket.onerror = (error) => {
-    console.log('WebSocket error: ', error);
-  };
+  const [socket, setSocket] = useState<Socket | null>(null); // State to hold the socket instance
 
   useEffect(() => {
-    console.log('WebSocket connection established.');
-    const messageHandler = (event: MessageEvent) => {
-      const message = event.data;
-      setMessages((prevMessages) => [...prevMessages, message]);
-    };
-    socket.addEventListener('message', messageHandler);
+    // Create the Socket.IO connection using io() function only once when the component mounts
+    const newSocket = io('http://localhost:5000'); // Change the port to match your backend WebSocket port
+    setSocket(newSocket);
+
+    // Add event listeners to handle Socket.IO events only once
+    newSocket.on('connect', () => {
+      console.log('Socket.IO connection established.');
+    });
+
+    newSocket.on('error', (errorEvent: Event) => {
+      console.log('Socket.IO error:', errorEvent);
+    });
+
+    newSocket.on('chatMessage', (messageData: MessageData) => {
+      setMessages((prevMessages) => [...prevMessages, messageData]);
+    });
+
+    // Clean up Socket.IO connection when the component unmounts
     return () => {
-      socket.removeEventListener('message', messageHandler);
-      socket.close();
+      if (newSocket) {
+        newSocket.disconnect();
+      }
     };
   }, []);
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    socket.send(inputValue);
+    if (socket) {
+      const messageData: MessageData = {
+        message: inputValue,
+        senderId: socket.id, // Add the sender's ID to the message data
+      };
+      socket.emit('chatMessage', messageData);
+    }
     setInputValue('');
   };
+  // ... (App component code)
+
+  // ... (existing code)
+
   return (
-    <div>
-      <h1>Chat Test</h1>
       <div>
-        {messages.map((message, index) => (
-          <p key={index}>{message}</p>
-        ))}
+        <h1>Chat Test</h1>
+        <div className="messages-container">
+          {messages.map((messageData, index) => (
+              <div
+                  key={index}
+                  className={
+                    socket && messageData.senderId === socket.id
+                        ? 'message-wrapper self'
+                        : 'message-wrapper others'
+                  }
+              >
+                {/* Add the label for the message sender */}
+                <div className="message-label">
+                  {socket && messageData.senderId === socket.id ? 'You' : messageData.senderId}
+                </div>
+                <div className="message">{messageData.message}</div>
+              </div>
+          ))}
+        </div>
+        <form onSubmit={handleSubmit}>
+          <input
+              type="text"
+              value={inputValue}
+              onChange={(event) => setInputValue(event.target.value)}
+              placeholder="Enter message..."
+          />
+          <button type="submit">Send</button>
+        </form>
       </div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={inputValue}
-          onChange={(event) => setInputValue(event.target.value)}
-          placeholder="Enter message"
-        />
-        <button type="submit">Send</button>
-      </form>
-    </div>
   );
+
+  /*return (
+      <div>
+        <h1>Chat Test</h1>
+        <div className="messages-container">
+          {messages.map((messageData, index) => (
+              <div
+                  key={index}
+                  className={
+                    socket && messageData.senderId === socket.id
+                        ? 'message-wrapper self'
+                        : 'message-wrapper others'
+                  } // Use senderId to check the message sender
+                >
+                <div className="message">{messageData.message}</div>
+              </div>
+          ))}
+        </div>
+        <form onSubmit={handleSubmit}>
+          <input
+              type="text"
+              value={inputValue}
+              onChange={(event) => setInputValue(event.target.value)}
+              placeholder="Enter message..."
+          />
+          <button type="submit">Send</button>
+        </form>
+      </div>
+  );*/
+
 };
+
 export default App;
+
+
+
 
